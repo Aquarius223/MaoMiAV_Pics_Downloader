971e5c513e051be0bc90e81072009d22508502f7
Pzqqt <821026875@qq.com>
2018-09-23 16:23:32 +0800

    New version v2.1.0

--- mmav.py
+++ mmav.py
@@ -13,7 +13,7 @@ from collections import OrderedDict
 from concurrent.futures import ThreadPoolExecutor
 from bs4 import BeautifulSoup
 
-__version__ = "v2.0.0"
+__version__ = "v2.1.0"
 
 class Maomiav():
 
